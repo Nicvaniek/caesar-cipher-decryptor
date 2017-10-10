40ffe26ebe97e7dd2b203d5934cf0c05f0fe3fc8
Nicolai van Niekerk <nicvaniek@gmail.com>
2017-10-10 23:47:00 +0200

    Something something fixed something

--- decrypt.py
+++ decrypt.py
@@ -8,10 +8,10 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # The encrypted message (ciphertext)
-# message = "2XQM5QN7A10QYUXXU104M0P59146T1R5TM4Q5M0PNQOM4QR7X01661NQ5QQ0NA6TQM7564MXUM0S18Q40YQ06M0P2XQM5QNQ8USUXM06"
+message = "2XQM5QN7A10QYUXXU104M0P59146T1R5TM4Q5M0PNQOM4QR7X01661NQ5QQ0NA6TQM7564MXUM0S18Q40YQ06M0P2XQM5QNQ8USUXM06"
 # message = "kMYM567PQ06M66TQw0U8Q45U6a1Rr4Q614UM567PaU0Se1Y276Q4uOUQ0OQ5U0OQECDH" # IamastudentattheUniversityofPretoriastudyingComputerSciencesince2015
 # message = "MJQQTYMJWJR3SFRJNXSTYPST1SG3mUJTUQJ" #hellotheremynameisnotknownby7people
-message = "5TQ3QM3QgzcevnAHDD53QQ4q7Q3vTQ3Q"
+# message = "5TQ3QM3QgzcevnAHDD53QQ4q7Q3vTQ3Q" #thereareEXACTLY400treesOverThere
 message_list = list(message)
 
 finals = []
@@ -58,7 +58,7 @@ print("Ciphertext: " + message)
 print("Best Guess Plaintext: " + best_effort)
 print("alpha key: " + str(len(alphabet) - exes[index]))
 print("digit key: " + str(len(alphabet) - jays[index]))
-print("Number of options: " + str(scores[best_effort]))
+print("Number of options: " + str(len(scores)))
 
 raw_input("\nPress ENTER to display more...")
 count = 0
